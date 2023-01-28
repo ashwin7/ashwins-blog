55d237c645e0990009de331a13c8dc3d2f5d4b09
Ashwin Saxena <ashwinsaxena7@gmail.com>
2023-01-29 03:29:51 +0530

    remove tag

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -14,7 +14,7 @@ const { name, homepage } = packageJSON
 const Layout: React.FC = ({ children }) => {
   const { theme, themeToggler } = useTheme()
   const { title, author } = useSiteMetadata()
-  const copyrightStr = `Copyright © ${author}. Built by `
+  const copyrightStr = `Copyright © ${author}.`
 
   return (
     <ThemeProvider theme={styledTheme}>
@@ -27,9 +27,6 @@ const Layout: React.FC = ({ children }) => {
         <Footer role="contentinfo">
           <Copyright aria-label="Copyright">
             {copyrightStr}
-            <RepoLink href={homepage} target="__blank">
-              {name}
-            </RepoLink>
           </Copyright>
         </Footer>
       </ThemeContext.Provider>
